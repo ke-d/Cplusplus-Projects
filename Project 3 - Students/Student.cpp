d4176a9c33927ddf3bee2604639ae773fc5c5b0b
mrdokenny <mrdokenny@gmail.com>
2016-10-05 17:30:33 -0700

    4 Arrays for each Sort

--- Project 3 - Students/Student.cpp
+++ Project 3 - Students/Student.cpp
@@ -47,16 +47,30 @@ int main() {
 	student[2] = {4765, "Aponge Bob", myDate(5,3,1992), 100};
 	student[3] = {1234, "Clone", myDate(5,3,1950), 75};
 	student[4] = {4321, "Clone2", myDate(2,1,1950), 60};
+
+	Student stuID[size];
+	Student stuAge[size];
+	Student stuName[size];
+	Student stuGrade[size];
+	copy(student, student + size, stuID);
+	copy(student, student + size, stuAge);
+	copy(student, student + size, stuName);
+	copy(student, student + size, stuGrade);
+
 	Student * stupt[4];
-	Student result[size];
-	copy(student, student + size, result);
-	stupt[0] = result;
-//	stupt[1] = &student[1];
-//	stupt[2] = &student[2];
-//	stupt[3] = &student[3];
+	stupt[0] = stuID;
+	stupt[1] = stuAge;
+	stupt[2] = stuName;
+	stupt[3] = stuGrade;
 	MergeSort::mergeSort(stupt[0], size,StudentComparators::compareByStudentID());
+	MergeSort::mergeSort(stupt[1], size,StudentComparators::compareByStudentAge());
+	MergeSort::mergeSort(stupt[2], size,StudentComparators::compareByStudentName());
+	MergeSort::mergeSort(stupt[3], size,StudentComparators::compareByStudentGrade());
 	printArrayOfStudents(student, size);
 	printArrayOfStudents(stupt[0], size);
+	printArrayOfStudents(stupt[1], size);
+	printArrayOfStudents(stupt[2], size);
+	printArrayOfStudents(stupt[3], size);
 
 	return 0;
 }
