c4b70a3d197cea749c2aef662afb2ce17926c91e
mrdokenny <mrdokenny@gmail.com>
2016-11-11 10:22:58 -0800

    STL: Made comments and Fixed Display

--- Project 6 - STL/STL.cpp
+++ Project 6 - STL/STL.cpp
@@ -17,23 +17,32 @@
 
 using namespace std;
 
-void generateNumbers(vector<int> & nums) {
-	for(int i = 0; i < 10000; ++i) {
+/**
+ * Generate numbers for a vector
+ */
+void generateNumbers(vector<int> & nums, int total) {
+	for(int i = 0; i < total; ++i) {
 		nums.push_back(rand() % 100 + 1);
 	}
 }
 
+/**
+ * Display the entire vector in columns
+ */
 void displayVector(vector<int> & nums) {
 	int count = 0;
 	for(vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
-		++count;
 		if(count % 50 == 0) {
 			cout << endl;
 		}
 		cout << left << setw(4) << (*it);
+		++count;
 	}
 }
 
+/**
+ * Calculate the total
+ */
 int calculateTotal(vector<int> & nums) {
 	int total = 0;
 	for(vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
@@ -42,10 +51,16 @@ int calculateTotal(vector<int> & nums) {
 	return total;
 }
 
+/**
+ * Calculate the average of an vector
+ */
 double calculateAverage(vector<int> & nums) {
 	return (double) calculateTotal(nums) / nums.size();
 }
 
+/**
+ * Gets the amount of times a number appears in the vector and stores it into a map
+ */
 void calculateNumTimesInList(vector<int> & nums, map<int, int> & times) {
 	for(vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
 		map<int,int>::iterator index = times.find(*it);
@@ -57,22 +72,30 @@ void calculateNumTimesInList(vector<int> & nums, map<int, int> & times) {
 	}
 }
 
+/**
+ * Prints out the map
+ */
 void printTimes(map<int, int> & times) {
 	int count = 0;
+	cout << left;
 	for(map<int, int>::iterator it = times.begin(); it != times.end(); ++it) {
 		++count;
+		if(count < 10) {
+			cout << it->first << ":" << setw(11) << it->second;
+		} else {
 		cout << it->first << ":" << setw(10) << it->second;
+		}
 		if(count % 10 == 0) {
 			cout <<endl;
 		}
 	}
 }
+
 int main() {
 	srand(time(NULL));
 	vector<int> nums;
-	generateNumbers(nums);
+	generateNumbers(nums, 10000);
 	sort(nums.begin(), nums.end());
-//	cout << nums.size();
 
 	displayVector(nums);
 	cout<<endl;
